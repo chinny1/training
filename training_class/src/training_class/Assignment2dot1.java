bdb633be8a465a7b4f0b4ba035765c1b790617b5
Chinny <chinny_akwarandu@gcitsolutions.com>
2015-04-15 01:37:55 -0400

    Just did part 1 of assignment 3. I put notes through the code to make it easier to read.

--- training_class/src/training_class/Assignment2dot1.java
+++ training_class/src/training_class/Assignment2dot1.java
@@ -5,6 +5,10 @@ import java.util.*;
 public class Assignment2dot1 {
 
 
+	public int getSize(Set mine){
+		
+		return mine.size();
+	}
 	
 	//public class SetDemo {
 
@@ -12,6 +16,8 @@ public class Assignment2dot1 {
 
 		    // Create a set called mySet
 		    Set mySet = new HashSet();
+		    
+		    Assignment2dot1 myClass = new Assignment2dot1();
 
 		    // Ensure that this set contains an interesting selection of fruit 
 		    String fruit1 = "pear", fruit2 = "banana", fruit3 = "tangerine",
@@ -26,6 +32,38 @@ public class Assignment2dot1 {
 		    // Display contents of mySet
 		    System.out.println( "mySet now contains:" );
 		    System.out.println( mySet );
+		    
+		    System.out.println("");//line break 
+		    
+		    //action: return size of set
+		    System.out.println( "mySet now has size: "+ myClass.getSize(mySet));
+		    
+		    //action: removing 2 fruits
+		    System.out.println("");//line break
+		    mySet.remove(fruit4);
+		    mySet.remove(fruit5);
+		    System.out.println( "mySet now contains:" );
+		    System.out.println( mySet );
+		    
+
+		    System.out.println("");//line break 
+		    
+		    //action: remove the rest of the fruits
+		    mySet.removeAll(mySet);
+		    
+		    System.out.println( "mySet now contains:" );
+		    System.out.println( mySet );
+		    
+		    //action: checking if set is empty
+		    System.out.println("");//line break 
+		    System.out.println( "is mySet empty ?" );
+		    if (mySet.isEmpty()){
+		    	
+		    	System.out.println( "Yes my set is empty" );
+		    }
+		    
+		    
+		    
 
 		 }
 	//}
